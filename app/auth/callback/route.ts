c40eab31a2f27712acbc89a9f44c767de41aa1d7
“Loren <couselm@gmail.com>
2025-01-06 21:58:58 +0800

    Update oauth code to be async

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the `@supabase/ssr` package. It exchanges an auth code for the user's session.
   const requestUrl = new URL(request.url);
-  const code = requestUrl.searchParams.get('code');
+  const code = await requestUrl.searchParams.get('code');
 
   if (code) {
     const supabase = await createClient();
